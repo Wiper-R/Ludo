eec9f8f009b21afa6a044e66e0147d82121bd03a
Wiper-R <rshivang12345@gmail.com>
2022-03-22 17:52:41 +0530

    Fixed animations and movement

--- Scenes/Token/Token.gd
+++ Scenes/Token/Token.gd
@@ -11,9 +11,10 @@ onready var initial_scale: Vector2 = scale;
 
 # -1 means in base.
 var current_position_idx: int = -1
-var is_moving: bool = false
 var _unused;
-
+var points_to_move: int = 0;
+var is_moving: bool = false;
+	
 
 func has_turn() -> bool:
 	return player.has_turn
@@ -27,7 +28,12 @@ func _ready() -> void:
 	_add_move_to_start_animation()
 
 	# Connecting Signals
-	_unused = animation_player.connect("animation_finished", self, "_animation_player_animation_finished")
+	animation_player.connect("animation_finished", self, "_animation_player_animation_finished")
+
+func _get_absolute_position_of_path(idx: int) -> Vector2:
+	var pos = player.get_path_curve().get_point_position(idx) - (player.global_position - board.global_position)
+	pos.y -= $Sprite.texture.get_size().y * $Sprite.scale.y / 2
+	return pos;
 
 
 func _add_move_to_start_animation():
@@ -38,11 +44,7 @@ func _add_move_to_start_animation():
 	var pos_track_idx = animation.add_track(Animation.TYPE_VALUE)
 	animation.track_set_path(pos_track_idx, ":position")
 	animation.track_insert_key(pos_track_idx, 0.00, position)
-	var new_pos: Vector2 = (
-		Vector2(start_point.x, start_point.y)
-		- (player.global_position - board.global_position)
-	)
-	new_pos.y -= $Sprite.texture.get_size().y * $Sprite.scale.y / 2
+	var new_pos: Vector2 = _get_absolute_position_of_path(player.global_home_path_index)
 	animation.track_insert_key(pos_track_idx, 0.75, new_pos)
 	
 	# Scale Animation
@@ -56,14 +58,15 @@ func _add_move_to_start_animation():
 
 
 func _move_to_start_point() -> void:
-	is_moving = true
 	animation_player.play("move_to_start_point")
-
 
 func _animation_player_animation_finished(anim_name: String) -> void:
 	if anim_name == "move_to_start_point":
-		current_position_idx = player.global_home_path_index
-		is_moving = false
+		current_position_idx = 0
+	elif anim_name == "do_move":
+		current_position_idx += points_to_move
+		points_to_move = 0
+		
 
 
 func _process(_delta: float) -> void:
@@ -72,3 +75,38 @@ func _process(_delta: float) -> void:
 		
 	if Input.is_action_just_pressed("ui_up") and is_in_base():
 		_move_to_start_point()
+
+	if Input.is_action_just_pressed("ui_down"):
+		move(4)
+
+func move(points: int):
+	points_to_move = points;
+	var animation = Animation.new();
+	var skip = 0.5;
+	var length = points * skip;
+	animation.set_length(length)
+
+	var pos_track_idx = animation.add_track(Animation.TYPE_VALUE)
+	var scale_track_idx = animation.add_track(Animation.TYPE_VALUE)
+	animation.track_set_interpolation_type(pos_track_idx, Animation.INTERPOLATION_CUBIC)
+	animation.track_set_interpolation_type(scale_track_idx, Animation.INTERPOLATION_CUBIC)
+	animation.track_set_path(pos_track_idx, ":position");
+	animation.track_set_path(scale_track_idx, ":scale");
+	
+	for i in range(0, points + 1):
+		animation.track_insert_key(pos_track_idx, i * skip, _get_absolute_position_of_path(current_position_idx + i))
+		animation.track_insert_key(scale_track_idx, i * skip + skip, initial_scale)
+		
+	
+	for i in range(0, points):
+		animation.track_insert_key(scale_track_idx, i * skip + skip / 1.6, initial_scale * 1.4)
+		
+	animation_player.add_animation("do_move", animation);
+	animation_player.play("do_move");
+
+func _on_MoveTimer_timeout() -> void:
+	current_position_idx += 1;
+	position = _get_absolute_position_of_path(current_position_idx);
+	
+
+
