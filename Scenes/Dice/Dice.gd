290c6b13bd3d2de45f5444779eb5710c81f87ba7
Wiper-R <rshivang12345@gmail.com>
2022-04-08 10:57:22 +0530

    Dice roll on click

--- Scenes/Dice/Dice.gd
+++ Scenes/Dice/Dice.gd
@@ -1,12 +1,16 @@
 extends Node2D
 onready var sprite: Sprite = get_node("Sprite");
+onready var animated_dice: Sprite = get_node("AnimatedDice");
+onready var animation_player: AnimationPlayer = animated_dice.get_node("AnimationPlayer");
 onready var rng = RandomNumberGenerator.new()
-
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
 
-func roll() -> void:
+func is_rolling() -> bool:
+	return animation_player.is_playing()
+
+func done_rolling() -> void:
 	var rolled = rng.randi_range(1, 6)
 	sprite.frame = rolled;
 	
@@ -15,12 +19,32 @@ func roll() -> void:
 
 # TODO: Receive a move signal and reset the dice
 	
+	
+func roll() -> void:
+	animation_player.play("roll")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	rng.randomize()
-
-
+	
+	# Reset Variables
+	reset()
+	
+func reset() -> void:
+	sprite.visible = true;
+	sprite.frame = 0;
+	animated_dice.visible = false;
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
+
+func _on_Area2D_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if !(event is InputEventMouseButton):
+		return
+		
+	if !event.pressed:
+		return
+		
+	roll()
