ddcdfc3cf32e6a4487c6623346552f0abeaa88e8
Wiper-R <rshivang12345@gmail.com>
2022-04-08 13:28:26 +0530

    Implement Turn Change

--- Scenes/Dice/Dice.gd
+++ Scenes/Dice/Dice.gd
@@ -51,7 +51,7 @@ func reset() -> void:
 #func _process(delta: float) -> void:
 #	pass
 
-func _on_Area2D_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func _on_Area2D_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if !(event is InputEventMouseButton):
 		return
 		
--- Scenes/Game/Game.gd
+++ Scenes/Game/Game.gd
@@ -1,16 +1,39 @@
 extends Node2D
+onready var players = get_node("Players");
+const MAX_PLAYERS = 4.0;
+const ALL_PLAYERS = ["Blue", "Red", "Green", "Yellow"];
+var current_players = [];
+var turn_idx = -1;
+
+func _assign_players(num: int) -> void:
+	assert (num > 1, "ERROR: Ludo requires atleast 2 players.")
+	
+	var skip: int = int(floor(MAX_PLAYERS / num));
+
+	for pidx in range(0, num * skip, skip):
+		current_players.push_back(ALL_PLAYERS[pidx])
+
+	for player in ALL_PLAYERS:
+		if not player in current_players:
+			get_node("Players/%s" % player).queue_free()
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+func switch_turn() -> void:
+	turn_idx += 1;
 
+	if turn_idx >= len(current_players):
+		turn_idx = 0 
 
-# Called when the node enters the scene tree for the first time.
+	for player in players.get_children():
+		player.has_turn = false;
+		
+	players.get_node(current_players[turn_idx]).has_turn = true;
+
 func _ready() -> void:
 	$Dice.block()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+	_assign_players(2)
+	switch_turn()
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_up"):
+		switch_turn()
